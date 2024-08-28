8737a5dcb9927caeb2434fc2c7cd480004069a5e
4tline <themineur57@gmail.com>
2024-08-28 14:00:17 +0200

    implémentation
    get default gateway

--- main().py
+++ main().py
@@ -1,4 +1,13 @@
-import os, platform
+import os, platform, netifaces
+
+def get_default_gateway():
+    # Recuperer les informations de la passerelle par defaut
+    gateways = netifaces.gateways()
+    # Extraire l'adresse IP de la passerelle par defaut pour IPv4
+    default_gateway = gateways.get('default', {}).get(netifaces.AF_INET, [None])[0]
+    print(f"La passerelle par defaut est : {default_gateway}")
+    return default_gateway
+
 def find_default_getaway(logi):
     match logi:
         case 'Linux':
@@ -6,7 +15,7 @@ def find_default_getaway(logi):
         case 'Darwin':
             return 2
         case 'Windows':
-            return 3
+            return get_default_gateway()
         case _:
             return 0
 def __main__():
@@ -14,9 +23,6 @@ def __main__():
     plat=os.name
     default_gateaway=find_default_getaway(logi)
     switch=True
-    while switch:
-        pass
-    
     return logi, plat, default_gateaway
 __main__()
 print(__main__())
